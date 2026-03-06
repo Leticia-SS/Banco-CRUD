b65c5d8b781a91b75fd1f932ba96d28fb765bee9
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-06 07:28:17 -0300

    initial commit

---
+++ src/main/java/br/edu/infnet/banco/BancoCrudApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/banco/BancoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
