970f45ea3842b0b4d50b3788df9ce7ba6b68e19d
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-06 08:12:15 -0300

    find all method for console

--- src/main/java/br/edu/infnet/banco/BancoCrudApplication.java
+++ src/main/java/br/edu/infnet/banco/BancoCrudApplication.java
@@ -1,11 +1,23 @@
 package br.edu.infnet.banco;
 
+import br.edu.infnet.banco.model.Conta;
+import br.edu.infnet.banco.service.ContaService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootApplication
 public class BancoCrudApplication implements CommandLineRunner {
+
+	private final ContaService contaService;
+
+	public BancoCrudApplication(ContaService contaService) {
+		this.contaService = contaService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancoCrudApplication.class, args);
@@ -13,6 +25,11 @@ public class BancoCrudApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Ola pessoal!!!");
+		consultarTodas();
+	}
+
+	public void consultarTodas() {
+		List<Conta> contas = contaService.consultarContas();
+		contas.forEach(conta -> System.out.println(conta));
 	}
 }
